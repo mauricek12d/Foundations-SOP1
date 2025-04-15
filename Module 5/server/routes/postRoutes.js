d317b3312a682b1a0fb066a313ff1d6ffbb02cd4
Maurice Zuniga <mauricek12d@gmail.com>
2025-04-15 00:43:36 -0400

    Added a middleware file and coded the authentication and authorizations middleware into it

--- Module 5/server/routes/postRoutes.js
+++ Module 5/server/routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getAllPosts, createPost } = require('../controllers/postController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', getAllPosts);
-router.post('/', createPost);
+router.post('/', authMiddleware, createPost);
 
 module.exports = router;
